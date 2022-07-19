9804a2e5c3e5dfaf50db4013bc4ad40eced30636
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-07-19 20:15:12 +0900

    ,,

--- diary/main10.js
+++ diary/main10.js
@@ -1,20 +1,22 @@
-window.addEventListener("load", function () {
+addEventListener("load", function () {
   generateList();
-  var now = new Date();
-  var y = now.getFullYear();
-  var m = now.getMonth() + 1;
-  var d = now.getDate();
+  now = new Date();
+  y = now.getFullYear();
+  m = now.getMonth();
+  d = now.getDate();
   document.getElementById("dialyDate").value = y + "年" + m + "月" + d + "日";
+
   document.getElementById("saveButton").addEventListener("click", function () {
     const dialyTitle = document.getElementById("dialyTitle").value;
     const dialyContent = document.getElementById("dialyContents").value;
     const dialyDate = document.getElementById("dialyDate").value;
     saveData = {
       title: dialyTitle,
-      content: dialyContent,
+      contents: dialyContent,
     };
+
     window.localStorage.setItem(dialyDate, JSON.stringify(saveData));
-    document.getElementById("status").innerHTML = "保存することができた";
+    document.getElementById("status").innerHTML = "保存することができました";
     generateList();
   });
   document
@@ -22,9 +24,9 @@ window.addEventListener("load", function () {
     .addEventListener("click", function () {
       var dialyDate = document.getElementById("dialyDate").value;
       if (localStorage.getItem(dialyDate) == null) {
-        alert("何もありません");
+        alert("データがありません");
       } else {
-        if (confirm("削除してもいいですか?")) {
+        if (confirm("データを消しますか?")) {
           localStorage.removeItem(dialyDate);
           document.getElementById("status").innerHTML =
             "削除することができました";
---
+++ diary/main9.js
@@ -0,0 +1,52 @@
+window.addEventListener("load", function () {
+  generateList();
+  var now = new Date();
+  var y = now.getFullYear();
+  var m = now.getMonth() + 1;
+  var d = now.getDate();
+  document.getElementById("dialyDate").value = y + "年" + m + "月" + d + "日";
+  document.getElementById("saveButton").addEventListener("click", function () {
+    const dialyTitle = document.getElementById("dialyTitle").value;
+    const dialyContent = document.getElementById("dialyContents").value;
+    const dialyDate = document.getElementById("dialyDate").value;
+    saveData = {
+      title: dialyTitle,
+      content: dialyContent,
+    };
+    window.localStorage.setItem(dialyDate, JSON.stringify(saveData));
+    document.getElementById("status").innerHTML = "保存することができた";
+    generateList();
+  });
+  document
+    .getElementById("deleteButton")
+    .addEventListener("click", function () {
+      var dialyDate = document.getElementById("dialyDate").value;
+      if (localStorage.getItem(dialyDate) == null) {
+        alert("何もありません");
+      } else {
+        if (confirm("削除してもいいですか?")) {
+          localStorage.removeItem(dialyDate);
+          document.getElementById("status").innerHTML =
+            "削除することができました";
+          generateList();
+        }
+      }
+    });
+});
+function generateList() {
+  var data = window.localStorage;
+  var dateList = "";
+  for (var i = 0; i < data.length; i++) {
+    var date = data.key(i);
+    dateList +=
+      `<a href="#" onclick=loadDialy("` + date + `")>` + date + `</a>` + `<br>`;
+  }
+  document.getElementById("list").innerHTML = dateList;
+}
+
+function loadDialy(dialyDate) {
+  var dialyData = JSON.parse(window.localStorage.getItem(dialyDate));
+  document.getElementById("dialyTitle").value = dialyData.title;
+  document.getElementById("dialyDate").value = dialyDate;
+  document.getElementById("dialyContents").value = dialyData.content;
+}
